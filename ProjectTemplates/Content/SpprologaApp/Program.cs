8684a75a2e1984b98f814eb967307b3301cd497a
jsakamoto <junichi.jp@gmail.com>
2021-01-03 14:41:43 +0900

    v.0.0.1-preview.4 release.

--- ProjectTemplates/Content/SpprologaApp/Program.cs
+++ ProjectTemplates/Content/SpprologaApp/Program.cs
@@ -19,7 +19,7 @@ namespace SpprologaApp
             builder.RootComponents.Add<App>("#app");
 
             builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-            builder.Services.AddSpprologa();
+            builder.Services.AddSpprologa().UseCSProlog();
 
             await builder.Build().RunAsync();
         }
